5a69e86c41a158ddb7917603c73f4195900658b9
bajo3 <you@example.com>
2023-04-08 22:13:17 -0300

    Calculadora finalizada

---
+++ js/app.js
@@ -0,0 +1,93 @@
+const displayValorAnterior = document.getElementById('valor-anterior');
+const displayValorActual = document.getElementById('valor-actual');
+const botonesNumeros = document.querySelectorAll('.numero');
+const botonesOperadores = document.querySelectorAll('.operador');
+
+const display = new Display(displayValorAnterior, displayValorActual);
+
+botonesNumeros.forEach(boton => {
+    boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML));
+});
+
+botonesOperadores.forEach(boton => {
+    boton.addEventListener('click', () => display.computar(boton.value))
+});
+
+
+/*Particulas*/
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+let particlesArray;
+
+// Set canvas width and height
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Particle object
+class Particle {
+    constructor(x, y, directionX, directionY, size, color) {
+        this.x = x;
+        this.y = y;
+        this.directionX = directionX;
+        this.directionY = directionY;
+        this.size = size;
+        this.color = color;
+    }
+
+    // Method to draw individual particles
+    draw() {
+        context.beginPath();
+        context.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
+        context.fillStyle = this.color;
+        context.fill();
+    }
+
+    // Check particle position, check mouse position, move the particle, draw the particle
+    update() {
+        // Check if particle is still within canvas
+        if (this.x + this.size > canvas.width || this.x - this.size < 0) {
+            this.directionX = -this.directionX;
+        }
+        if (this.y + this.size > canvas.height || this.y - this.size < 0) {
+            this.directionY = -this.directionY;
+        }
+
+        // Move particle
+        this.x += this.directionX;
+        this.y += this.directionY;
+
+        // Draw particle
+        this.draw();
+    }
+}
+
+// Function to create particle array
+function init() {
+    particlesArray = [];
+    const numberOfParticles = (canvas.height * canvas.width) / 15000;
+
+    for (let i = 0; i < numberOfParticles; i++) {
+        const size = (Math.random() * 5) + 1;
+        const x = (Math.random() * ((innerWidth - size * 2) - (size * 2)) + size * 2);
+        const y = (Math.random() * ((innerHeight - size * 2) - (size * 2)) + size * 2);
+        const directionX = (Math.random() * 5) - 5.5;
+        const directionY = (Math.random() * 5) - 3.5;
+        const color = '#FFFFFF';
+
+        particlesArray.push(new Particle(x, y, directionX, directionY, size, color));
+    }
+}
+
+// Animation loop
+function animate() {
+    requestAnimationFrame(animate);
+    context.clearRect(0, 0, innerWidth, innerHeight);
+
+    for (let i = 0; i < particlesArray.length; i++) {
+        particlesArray[i].update();
+    }
+}
+
+init();
+animate();
+
